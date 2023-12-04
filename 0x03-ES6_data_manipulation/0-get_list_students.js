1ea9f6982d15b4d38a847524924d3507eaa08a90
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-04 10:17:10 +0300

    ESC data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,14 @@
+// function to be getListStudents
+
+function getListStudents () {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
+// function to retrieve list of students
+// array of student objects
+// each student object has properties id, firstName, and location
+
+export default getListStudents;
