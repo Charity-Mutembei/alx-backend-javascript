ec00a9956d6d66b304f6d9075070bbb79d82445d
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-04 20:00:34 +0300

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
-import getListStudents from './0-get_list_students';
+// import { getListStudents } from './0-get_list_students.js';
 
-export default function updateStudentGradeByCity (city, newGrades) {
-  const students = getListStudents(); // Assuming getListStudents is defined
+export default function updateStudentGradeByCity (studentsArray, city, newGrades) {
+  const students = studentsArray;
 
   return students
     .filter(student => student.location === city)
