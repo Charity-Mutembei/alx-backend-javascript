71dca40e825fb7aefb2d0299a42a9f7f588cf13b
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-04 18:19:30 +0300

    task 0 - debug

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,11 @@
-function getListStudentIds(students) {
-    /*Check if the input is an array*/
-    if (!Array.isArray(students)) {
-        return [];
-    }
+function getListStudentIds (students) {
+  /* Check if the input is an array */
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
-    /*Use the map function to extract ids*/
-    return students.map(student => student.id);
+  /* Use the map function to extract ids */
+  return students.map(student => student.id);
 }
 
 export default getListStudentIds;
