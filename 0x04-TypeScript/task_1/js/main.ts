0643ea285eb2acbbb9c053eb34a90d9b4e40dc08
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-07 11:55:34 +0300

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -44,3 +44,31 @@ const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
     const fullName = `${firstLetter}. ${lastName}`;
     return fullName;
 };
+
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
