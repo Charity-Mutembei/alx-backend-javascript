7f52cd6644c9ed9e2d18819c5b3b67c413a72779
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-07 11:49:11 +0300

    task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,24 @@
+class Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+  
+    constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, yearsOfExperience: number | undefined, location: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.fullTimeEmployee = fullTimeEmployee;
+      this.yearsOfExperience = yearsOfExperience;
+      this.location = location;
+    }
+  
+    addAttribute<K extends string>(name: K, value: any) {
+      if (name in this) {
+        this[name] = value;
+        //[Type 'K' cannot be used to index type 'this'.ts(2536)(parameter) name: K extends string]
+      } else {
+        console.error(`Property '${name}' does not exist on Teacher.`);
+      }
+    }
+  }
