6532cb5222af614b41cc01419864f16f6de71129
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-19 23:39:17 +0300

    task 1

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   const IteratorObject = [];
 
-  for (const value of Object.values(report.allEmployees)) {
+  for (const value of Object.values(report.Employees)) {
     IteratorObject.push(...value);
   }
 
