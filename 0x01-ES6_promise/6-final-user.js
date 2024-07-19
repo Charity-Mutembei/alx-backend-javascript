bab22da10ec1dbc9b13640a789567005f78e7d98
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-20 00:51:07 +0300

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,15 +4,15 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const res = [];
   try {
-    const user = await signUpUser(firstName, lastName);
-    res.push({ status: 'success', value: user });
+    const userSigned = await signUpUser(firstName, lastName);
+    res.push({ status: 'success', value: userSigned });
   } catch (error) {
     res.push({ status: 'failed', value: error.toString() });
   }
 
   try {
-    const photo = await uploadPhoto(fileName);
-    res.push({ status: 'success', value: photo });
+    const photoUpload = await uploadPhoto(fileName);
+    res.push({ status: 'success', value: photoUpload });
   } catch (error) {
     res.push({ status: 'failed', value: error.toString() });
   }
