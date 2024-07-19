93a97fb664c55dbefdb0da7c3ec7f0fe51bb1b75
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-19 23:32:40 +0300

    task 1

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const obj = {
+  const object = {
     [departmentName]: employees,
   };
-  return obj;
+  return object;
 }
