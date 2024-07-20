99ffab0d12d824c4a3eea388d70c599dc81aa71f
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-20 14:16:38 +0300

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Square footage must be a number');
+    if (
+      new.target !== Building
+        && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
-
     this._sqft = sqft;
-
-    if (new.target === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly');
-    }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
