d39ed56b7bc5f1f5914126a141eaaa5c239104dc
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-20 13:00:25 +0300

    tas 2

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,14 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const classesArray = [];
   const room1 = new ClassRoom(19);
   const room2 = new ClassRoom(20);
   const room3 = new ClassRoom(34);
 
-  classes.push(room1);
-  classes.push(room2);
-  classes.push(room3);
-
-  return classesArray;
+  return [room1, room2, room3];
 }
