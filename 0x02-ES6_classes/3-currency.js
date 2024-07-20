16fa1ce86963eb618381f482d4b7b790d9283664
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-20 13:17:19 +0300

    task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,39 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-      if (typeof code !== 'string') {
-          throw new TypeError('Code must be a string');
-      }
-      if (typeof name !== 'string') {
-          throw new TypeError('Name must be a string');
-      }
-
-      this._code = code;
-      this._name = name;
+    this.code = code;
+    this.name = name;
   }
 
   get code() {
-      return this._code;
-  }
-
-  set code(value) {
-      if (typeof value !== 'string') {
-          throw new TypeError('Code must be a string');
-      }
-      this._code = value;
+    return this._code;
   }
 
   get name() {
-      return this._name;
+    return this._name;
   }
 
-  set name(value) {
-      if (typeof value !== 'string') {
-          throw new TypeError('Name must be a string');
-      }
-      this._name = value;
+  set code(code) {
+    this._code = code;
+  }
+
+  set name(name) {
+    this._name = name;
   }
 
   displayFullCurrency() {
-      return `${this._name} (${this._code})`;
+    return `${this._name} (${this._code})`;
   }
 }
