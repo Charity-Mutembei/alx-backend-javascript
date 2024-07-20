6552fa6fccb13ce51b6696c7d8ce09c5e9269e3d
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-20 15:23:17 +0300

    task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,15 +1,40 @@
+const cloneSymbol = Symbol('clone');
+
 export default class Car {
   constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw new TypeError('Brand must be a string');
+    }
+    if (typeof motor !== 'string') {
+      throw new TypeError('Motor must be a string');
+    }
+    if (typeof color !== 'string') {
+      throw new TypeError('Color must be a string');
+    }
+
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  static get [Symbol.species]() {
-    return this;
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
   }
 
   cloneCar() {
-    return new this.constructor[Symbol.species]();
+    const NewCar = this[cloneSymbol]();
+    return new NewCar();
+  }
+
+  [cloneSymbol]() {
+    return this.constructor;
   }
 }
